49b23dad58766df7edd196c837c40343ed5a53ab
sean <sstewart424@gmail.com>
2017-12-11 03:28:06 +0000

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+prem = League.where(title: 'Premier League').first
+prem.delete if prem
+
+prem = League.create([title: 'Premier League'])
